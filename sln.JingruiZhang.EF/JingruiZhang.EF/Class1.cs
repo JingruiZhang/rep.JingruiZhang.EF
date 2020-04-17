15e1af66e9f89c4224f20f6d144f999a05a011a6
JingruiZhang <ruilovechu@live.com>
2020-04-17 17:18:30 +0800

    在.netstandard项目中添加各平台的引用

--- sln.JingruiZhang.EF/JingruiZhang.EF/Class1.cs
+++ sln.JingruiZhang.EF/JingruiZhang.EF/Class1.cs
@@ -5,10 +5,12 @@ namespace JingruiZhang.EF
 #if NET45
     public class Class1
     {
+        System.Data.Entity.DbContext c;
     }
 #else
     public class Class1
     {
+        Microsoft.EntityFrameworkCore.DbContext c;
     }
 #endif
 }
